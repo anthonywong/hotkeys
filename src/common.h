8c85af82c3534585dcbf06855267aad4347b5f16
Anthony Wong <ypwong@ypwong.org>
2001-03-14 17:33:53 +0000

    Added copyright+license message

--- src/common.h
+++ src/common.h
@@ -4,6 +4,7 @@
    Adapted for autoproject by James R. Van Zandt.
 
    Copyright (C) 89, 91, 92, 93, 94, 1996 Free Software Foundation, Inc.
+   Copyright (C) 2000,2001  Anthony Y P Wong <ypwong@ypwong.org>
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
